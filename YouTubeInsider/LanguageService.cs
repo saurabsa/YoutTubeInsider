575ca5037f3a52de06048d33f6f1a46453afe628
saurabsa <saurabsa@microsoft.com>
2016-07-27 11:43:38 -0700

    Added more keywords in LanguageService

--- YouTubeInsider/LanguageService.cs
+++ YouTubeInsider/LanguageService.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace YouTubeInsider
 {
-    class LanguageService
+    public class LanguageService
     {
         private static string[] JavaKeywords = { "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue",
         "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for", "goto", "if", "implements",
         "import", "instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public",
         "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this", "throw", "throws", "transient",
-        "try", "void", "volatile", "while", "false", "null", "true" };
+        "try", "void", "volatile", "while", "false", "null", "true", "java" };
 
         private static string[] CSharpKeywords = { "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked",
         "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit",
@@ -21,7 +21,7 @@ namespace YouTubeInsider
         "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc",
         "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked",
         "unsafe", "ushort", "using", "var", "virtual", "void", "volatile", "while", "add", "alias", "get", "global", "partial",
-        "remove", "set", "value", "where", "yield"};
+        "remove", "set", "value", "where", "yield", "c#", "cs", "csharp"};
 
         private static string[] CPlusPlusKeywords = { "alignas", "alignof", "and", "and_eq", "asm", "atomic_cancel", "atomic_commit", "atomic_noexcept", "auto",
         "bitand", "bitor", "bool", "break", "case", "catch", "char", "char16_t", "char32_t", "class", "compl", "concept", "const",
@@ -31,7 +31,7 @@ namespace YouTubeInsider
         "requires", "return", "short", "signed", "sizeof", "static", "static_assert", "static_cast", "struct", "switch", "synchronized", "template",
         "this", "thread_local", "throw", "true", "try", "typedef", "typeid", "typename", "union", "unsigned", "using", "virtual", "void", "volatile",
         "wchar_t", "while", "xor", "xor_eq", "override", "final", "transaction_safe", "transaction_safe_dynamic ", "if", "elif", "else",
-        "endif", "defined", "ifdef", "ifndef", "define", "undef", "include", "line", "error", "pragma"};
+        "endif", "defined", "ifdef", "ifndef", "define", "undef", "include", "line", "error", "pragma", "cpp"};
 
         private static string[] CKeywords = { "auto", "break", "case", "char", "const", "continue", "default", "do", "double", "else", "enum", "extern",
         "float", "for", "goto", "if", "int", "long", "register", "return", "short", "signed", "sizeof", "static", "struct", "switch",
@@ -39,30 +39,30 @@ namespace YouTubeInsider
 
         private static string[] PythonKeywords = { "and", "del", "from", "not", "while", "as", "elif", "global", "or", "with", "assert", "else", "if", "pass",
         "yield", "break", "except", "import", "print", "class", "exec", "in", "raise", "continue", "finally", "is", "return", "def", "for", "lambda",
-        "try", "False", "True", "None", "nonlocal"};
+        "try", "False", "True", "None", "nonlocal", "python"};
 
         public static string analyzeType(List<string> textWords, string videoName)
         {
             uint javaPoints = 0, cppPoints = 0, cSharpPoints = 0, cPoints = 0, pythonPoints = 0;
             foreach (string word in textWords)
             {
-                if(JavaKeywords.Contains(word))
+                if(JavaKeywords.Contains(word.ToLower()))
                 {
                     javaPoints++;
                 }
-                if (CSharpKeywords.Contains(word))
+                if (CSharpKeywords.Contains(word.ToLower()))
                 {
                     cSharpPoints++;
                 }
-                if (CPlusPlusKeywords.Contains(word))
+                if (CPlusPlusKeywords.Contains(word.ToLower()))
                 {
                     cppPoints++;
                 }
-                if (CKeywords.Contains(word))
+                if (CKeywords.Contains(word.ToLower()))
                 {
                     cPoints++;
                 }
-                if (PythonKeywords.Contains(word))
+                if (PythonKeywords.Contains(word.ToLower()))
                 {
                     pythonPoints++;
                 }
